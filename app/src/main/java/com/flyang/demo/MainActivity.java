8b3d1798536c1ddeebb422b830b9da442f79b49b
曹阳飞 <cao18317713272@163.com>
2019-07-16 22:02:01 +0800

    Initial commit

---
+++ app/src/main/java/com/flyang/demo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.flyang.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Button(View view) {
+//        LogUtils.d("测试打印日志");
+    }
+}
